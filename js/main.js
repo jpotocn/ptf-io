19977d48730a02ade425dc6aca9077fbde6db9be
janp <jan.potocnik@solvera-lynx.com>
2019-04-26 11:32:43 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-nav');
+const menuBtn = document.querySelector('.menu-branding');
