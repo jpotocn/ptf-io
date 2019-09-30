1180271dd0e93879763527cc9021e2867b2b6b25
Jan <jpotocn@gmail.com>
2019-09-30 11:26:19 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
+//Select DOM items
 const menuBtn = document.querySelector('.menu-btn');
-const menuBtn = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu-nav');
-const menuBtn = document.querySelector('.menu-branding');
+const menu = document.querySelector('.menu');
+const menuLine = document.querySelectorAll('.menu-line');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menuBranding.classList.add('show');
+        menuLine.forEach(item => item.classList.add("show"));
+        navItems.forEach(item => item.classList.add('show'));
+        menuNav.classList.add('show');
+        menu.classList.add('show');
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menuBranding.classList.remove('show');
+        menuLine.forEach(item => item.classList.remove("show"));
+        navItems.forEach(item => item.classList.remove('show'));
+        menuNav.classList.remove('show');
+        menu.classList.remove('show');
+        showMenu = false;
+    }
+}
